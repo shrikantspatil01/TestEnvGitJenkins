9980e9f0e6760725eca1f5695369fc3bdb169476
shrikpat <shrikpat@LIN59017876.corp.capgemini.com>
2024-01-23 00:01:26 +0530

    added two syso comments from thinkpad

--- src/main/java/com/pages/Test3.java
+++ src/main/java/com/pages/Test3.java
@@ -5,6 +5,8 @@ public class Test3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Java Life");
+		System.out.println("Hello collection Life");
+		System.out.println("Hello OOP concepts Life");
 	}
 
 }
